161524c94ff6b6bc93ea2a76820c40682788d80b
Kristen Shea <kstar61@live.com>
2018-08-14 23:10:37 -0500

    *

--- CG 6-3 Alice in wonderland/Program.cs
+++ CG 6-3 Alice in wonderland/Program.cs
@@ -10,19 +10,49 @@ namespace CG_6_3_Alice_in_wonderland
              * and then prompt the user for a string to search for within this string. Print whether the search 
              * term was found. See if you can make the search case-insensitive, so that searching for “alice” prints true.*/
 
+            //explain program and prompt user for word to search in one line of code using \n to add a line break and 
+            //using Console.Write so user's entry is on the same line as the second sentence.
+            Console.Write("Search for words in the very first sentence in Alice in Wonderland. \nEnter word you would like to search: ");
 
-            string alice = "Alice was beginning to get very tired of sitting by her sister on the bank, " +
+            string toSearch = Console.ReadLine();
+
+           string alice = "Alice was beginning to get very tired of sitting by her sister on the bank, " +
                 "and of having nothing to do: once or twice she had peeped into the book her sister was " +
                 "reading, but it had no pictures or conversations in it, ‘and what is the use of a book,’ " +
                 "thought Alice ‘without pictures or conversation?’ ";
 
 
             
+            StringComparison search = StringComparison.OrdinalIgnoreCase;
+
+            
+            
+        
 
 
 
 
+
+
+
+
+
+        Console.ReadLine();
 
             }
+
+       // private static string Contains(string toSearch, StringComparison search, string alice)
+        //{
+           
+            
+
+           
+
+
+
+        //}
+
+
+
     }
 }
