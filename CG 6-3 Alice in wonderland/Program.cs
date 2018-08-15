7944da5803b900d67396c7088ae5139463f44d82
Kristen Shea <kstar61@live.com>
2018-08-14 21:04:06 -0500

    *

--- CG 6-3 Alice in wonderland/Program.cs
+++ CG 6-3 Alice in wonderland/Program.cs
@@ -6,7 +6,23 @@ namespace CG_6_3_Alice_in_wonderland
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            /*The first sentence of Alice in Wonderland is below. Store this sentence in a string, 
+             * and then prompt the user for a string to search for within this string. Print whether the search 
+             * term was found. See if you can make the search case-insensitive, so that searching for “alice” prints true.*/
+
+
+            string alice = "Alice was beginning to get very tired of sitting by her sister on the bank, " +
+                "and of having nothing to do: once or twice she had peeped into the book her sister was " +
+                "reading, but it had no pictures or conversations in it, ‘and what is the use of a book,’ " +
+                "thought Alice ‘without pictures or conversation?’ ";
+
+
+            
+
+
+
+
+
+            }
     }
 }
